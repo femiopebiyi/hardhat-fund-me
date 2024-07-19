8ecdb0533387d9e32108d76a92179a9deb1dd7df
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-07-19 14:13:06 +0100

    how to deploy mock pricefeeds

--- deploy/01-fundMe-deploy.ts
+++ deploy/01-fundMe-deploy.ts
@@ -4,7 +4,7 @@ import { developmentChains, networkConfig } from "../helper-hardhat-config";
 import { get } from "http";
 
 
- export default async function deployFunc(hre: HardhatRuntimeEnvironment){
+const  deployFunc = async (hre: HardhatRuntimeEnvironment) => {
     const {getNamedAccounts, deployments} = hre
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
@@ -15,12 +15,23 @@ import { get } from "http";
 let ethUsdPriceFeedAddress;
 
 if(developmentChains.includes(network.name)){
-    const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address
+} else{
+    ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
 }
 
     // when going for localhost or hardhat netw0rk we want to use a mock
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: []
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+
     })
+
+    log("-------------------------------------------------------------------------------------------------------------------- fund me deployed")
  }
+
+ export default deployFunc;
+
+ deployFunc.tags = ["all", "fundme"]
